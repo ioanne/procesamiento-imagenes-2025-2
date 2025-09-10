c06d02c58c68c6106e5aec9dbec5f06ad6e6a93e
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-09-10 20:01:57 -0300

    clase sinc

---
+++ procesamiento_imagenes/cosas_random.py
@@ -0,0 +1,11 @@
+# Como creamos nuestra propia ayuda?
+class ClaseConAyuda:
+    """
+    Esta es una clase de ejemplo que tiene una ayuda.
+    """
+    def __init__(self):
+        """Ayuda del metodo self"""
+        pass
+    def metodo_con_ayuda(self) -> None:
+        """Ayuda del metodo metodo_con_ayuda"""
+        pass
--- procesamiento_imagenes/ejemplo_basico.py
+++ procesamiento_imagenes/ejemplo_basico.py
@@ -1,3 +1,14 @@
 # opencv
 # Pueden buscarlo en pypi.org (googleando "pypi opencv")
 
+import cv2
+
+imagen = cv2.imread("imagenes/naranja.jpg")
+
+# podemos usar dir para ver los atributos y metodos de un objeto
+dir(imagen) # El dir no funciona solo para el objeto principal, sino para cualquier cosa.
+dir(imagen.diagonal) # diagonal es un atributo de imagen
+
+# Tambien podemos ver la ayuda de un objeto
+help(imagen)
+
