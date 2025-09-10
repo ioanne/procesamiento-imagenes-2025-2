d6a8df3960633b32dc8deed87ae093abd3184f9e
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-09-10 20:11:16 -0300

    clase sinc

--- procesamiento_imagenes/ejemplo_basico.py
+++ procesamiento_imagenes/ejemplo_basico.py
@@ -6,9 +6,12 @@ import cv2
 imagen = cv2.imread("imagenes/naranja.jpg")
 
 # podemos usar dir para ver los atributos y metodos de un objeto
-dir(imagen) # El dir no funciona solo para el objeto principal, sino para cualquier cosa.
-dir(imagen.diagonal) # diagonal es un atributo de imagen
+# dir(imagen) # El dir no funciona solo para el objeto principal, sino para cualquier cosa.
+# dir(imagen.diagonal) # diagonal es un atributo de imagen
 
 # Tambien podemos ver la ayuda de un objeto
-help(imagen)
+# help(imagen)
 
+cv2.imshow("Naranja", imagen)
+# cv2.imwrite("imagenes/naranja_copia.jpg", imagen)
+cv2.waitKey(0)
